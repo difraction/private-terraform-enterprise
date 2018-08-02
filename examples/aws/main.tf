f6285890194fa50083c2ff98222f74a2915ae618
rberlind <roger@hashicorp.com>
2018-08-01 20:28:01 -0400

    final changes for Customer J

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -13,7 +13,7 @@ module "pes" {
   aws_instance_ami       = "${var.aws_instance_ami}"
   aws_instance_type      = "${var.aws_instance_type}"
   ebs_device_name        = "${var.ebs_device_name}"
-  subnet_ids             = ["${var.subnet_ids}"]
+  subnet_ids             = ["${split(",", var.subnet_ids)}"]
   security_group_id = "${var.security_group_id}"
   ssh_key_name           = "${var.ssh_key_name}"
   database_pwd           = "${var.database_password}"
